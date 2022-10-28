129bca1b171a0a6f7b76d5fa35aece43824d8e6f
fairy-angel <fairy.angelic113@gmail.com>
2022-10-28 11:16:12 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 root "pages#index"
-get "index", to: "pages#index"
+get "/index", to: "pages#index"
 
-# ch ROUTES
-get "commercial", to:"pages#commercial"
-get "quoteform", to:"pages#quoteform"
-get "residential", to:"pages#residential"
+get "/commercial", to:"pages#commercial"
+get "/quoteform", to:"pages#quoteform"
+get "/residential", to:"pages#residential"
 
 
 end
